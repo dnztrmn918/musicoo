5763165a5088f67f3a225d05967c617d27258406
dnztrmn918 <dnztrmnn@icloud.com>
2026-03-19 03:10:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    # API_ID yoksa veya hatalıysa botun çökmesini engellemek için kontrol
     API_ID = int(os.getenv("API_ID", "0"))
     API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    
+    # SADECE BURAYI DEĞİŞTİRİYORUZ. Kodu direkt içine gömüyoruz:
+    BOT_TOKEN = "8372013267:AAHAcPRWYVclQRsePZVmPwBFqzwP8yfXS8g" 
+    
     STRING_SESSION = os.getenv("STRING_SESSION", "")
 
     if not API_ID or not API_HASH:
