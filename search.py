e0ef512914c1ad9d2f057bd95e1792e5fc4f3c7e
dnztrmn918 <dnztrmnn@icloud.com>
2026-03-27 03:14:35 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,19 +1,19 @@
 import yt_dlp
 
 def search_youtube(query):
-    # Çerez dosyasına, bot ayarlarına GEREK YOK!
-    # Sadece SoundCloud üzerinden temiz ve hızlı arama yapacağız.
+    # Çerez dosyasına GEREK YOK!
+    # Youtube'u atlıyor, SoundCloud'da arıyoruz.
     ydl_opts = {
         'format': 'bestaudio/best',
         'noplaylist': True,
         'quiet': True,
-        'default_search': 'scsearch', # SİHİRLİ KELİME: YouTube yerine SoundCloud'da ara
+        'default_search': 'scsearch', # SİHİRLİ KİŞİM: Youtube yerine SoundCloud
         'nocheckcertificate': True,
     }
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         if not query.startswith("http"):
-            query = f"scsearch:{query}" # scsearch = SoundCloud Search
+            query = f"scsearch:{query}"
             
         info = ydl.extract_info(query, download=False)
         
@@ -24,6 +24,6 @@ def search_youtube(query):
             'title': info.get('title'),
             'duration': info.get('duration_string', 'Bilinmiyor'),
             'thumbnail': info.get('thumbnail'),
-            'url': info.get('url'), # Doğrudan ses dosyası
-            'webpage_url': info.get('webpage_url', '#')
+            'url': info.get('url'), # Oynatılacak ham ses dosyası
+            'webpage_url': info.get('webpage_url', 'https://soundcloud.com')
         }
