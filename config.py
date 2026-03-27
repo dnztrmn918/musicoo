6d189ee0ab92bffc222c50b27eca6f394eddbd96
dnztrmn918 <dnztrmnn@icloud.com>
2026-03-27 04:23:06 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,8 @@ import os
 API_ID = int(os.getenv("API_ID", "0"))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-SESSION = os.getenv("SESSION", "") # Userbot oturumu (PyTgCalls için şart)
+SESSION = os.getenv("SESSION", "") 
+
+# YENİ EKLENEN VERİTABANI VE YETKİ DEĞİŞKENLERİ
+MONGO_DB_URI = os.getenv("MONGO_DB_URI", "")
+OWNER_ID = int(os.getenv("OWNER_ID", "0")) # Kendi Telegram ID'ni buraya yazacaksın
